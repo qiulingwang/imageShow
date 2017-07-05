0caca1841bca3452e3ea8ba62bef00b8daa95adc
shawn <wangqiuling@xianzhongwang.com>
2017-07-05 17:11:38 +0800

    update files

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 require('styles/main.styl');
 
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
 //加载json数据，并为每个json对象赋予imageUrl属性
 let imageDatas = require('../data/imageDatas.json');
@@ -25,15 +26,100 @@ class ImgFigure extends Component {
 }
 
 class AppComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.Constant = {
+      centerPos: {
+        left: 0,
+        right: 0
+      },
+      hPosRange: {     //水平方向取值范围
+        leftSecX: [0, 0],
+        rightSecX: [0, 0],
+        y: [0, 0]
+      },
+      vPosRange: {     //垂直方向的取值范围
+        x: [0, 0],
+        topY: [0, 0]
+      }
+    };
+    this.state = {
+      imgsArrangeArr: [
+        // {
+        //   pos: {
+        //     left: '0',
+        //     top: '0'
+        //   },
+        //   rotate: 0, //旋转角度
+        //   isInverse: false,//正反面
+        //   isCenter: false //图片是否居中
+        // }
+      ]
+    }
+  }
+
+  //重新布局所有图片
+  rearrange(centerIndex) {
+
+  }
+
+  componentDidMount() {
+    let stageDOM = ReactDOM.findDOMNode(this.refs.stage),
+      stageW = stageDOM.scrollWidth,
+      stageY = stageDOM.scrollHeight,
+      halfStageW = Math.ceil(stageW / 2),
+      halfStageH = Math.ceil(stageH / 2);
+
+    let imgFigureDOM = ReactDOM.findDOMNode(this.refs.imgFigure0),
+      imgW = imgFigureDOM.scrollWidth,
+      imgH = imgFigureDOM.scrollHeight,
+      halfImgW = Math.ceil(imgW / 2),
+      halfImgH = Math.ceil(imgH / 2);
+
+    //计算中心图片的位置点
+    this.Constant.centerPos = {
+      left: halfStageW - halfImgW,
+      top: halfStageH - halfImgH
+    }
+    //计算左侧,右侧区域图片排布的取值范围
+    this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+    this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
+
+    this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+    this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+
+    this.Constant.hPosRange.y[0] = -halfImgH;
+    this.Constant.hPosRange.y[1] = stageH - halfImgH;
+    //计算上测区域图片排布的取值范围
+    this.Constant.vPosRange.topY[0] = -halfImgH;
+    this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+
+    this.Constant.vPosRange.x[0] = halfStageW - imgW;
+    this.Constant.vPosRange.x[1] = halfStageW;
+
+    let num = Math.floor(Math.random() * imageDatas.length);
+    this.rearrange(num);
+  }
 
   render() {
-    let controllerUnits = [],imgFigures=[];
-    imageDatas.map((val,index) => {
-      imgFigures.push(<ImgFigure data={val} key={index} />)
+    let controllerUnits = [], imgFigures = [];
+    imageDatas.map((val, index) => {
+      if (!this.state.imgsArrangeArr[index]) {
+        this.state.imgsArrangeArr[index] = {
+          pos: {
+            left: 0,
+            top: 0
+          },
+          rotate: 0,
+          isInverse: false,
+          isCenter: false
+        }
+      }
+      imgFigures.push(<ImgFigure data={val} key={index} ref={'imgFigure' + index}/>)
     })
 
     return (
-      <section className="stage">
+      <section className="stage" ref="stage">
         <section className="img-sec">
           {imgFigures}
         </section>
